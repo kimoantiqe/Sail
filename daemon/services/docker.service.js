fdf72bf6ca01621de4ca422fc2b6ba93edb25970
Karim <kimoantiqe@hotmail.com>
2019-01-04 04:24:10 +0400

    Added docker service core

---
+++ daemon/services/docker.service.js
@@ -0,0 +1,30 @@
+const buildDockerImage = async function(req, res){
+    //Todo
+}
+module.exports.buildDockerImage = buildDockerImage;
+
+
+const startDockerContainer= async function(req, res){
+    //Todo
+}
+module.exports.startDockerContainer = startDockerContainer;
+
+const stopDockerContainer = async function(req, res){
+    //Todo
+}
+module.exports.stopDockerContainer = stopDockerContainer;
+
+const removeDockerContainer = async function(req, res){
+    //Todo
+}
+module.exports.removeDockerContainer = removeDockerContainer;
+
+const statusDockerContainer = async function(req, res){
+    //Todo
+}
+module.exports.statusDockerContainer = statusDockerContainer;
+
+const restartDockerContainer = async function(req, res){
+    //Todo
+}
+module.exports.restartDockerContainer = restartDockerContainer;
